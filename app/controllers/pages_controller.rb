0bf60d735a273a8a06ea4caa59a6cc3744a76145
Jessica Sumner <jess.r.sumner@gmail.com>
2015-10-01 20:30:26 -0400

    Available stat now reading from petfinder. List of names and photos of available rats showing on adopt page. Not formatted yet.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,20 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def about
-  end
-
-  def resources
-  end
-
-  def contact
-  end
-
-  def volunteer
-  end
-
   def adopt
+    @available_rats = available_rats
   end
 
-  def donate
+  private
+
+  def petfinder
+    @_petfinder ||= Petfinder::Client.new
   end
 
-  def newsletters
+  def any_rat_rescue
+    @_any_rat_rescue ||= petfinder.shelter("AZ173")
   end
+
+  def available_rats
+    @_available_rats ||= petfinder.shelter_pets(any_rat_rescue.id)
+  end
+
 end
