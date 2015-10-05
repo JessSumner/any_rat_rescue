4d61c78c4946f93e5bbb52d026a205d901addfd4
Jessica Sumner <jess.r.sumner@gmail.com>
2015-10-05 12:57:56 -0400

    Resources controller added.
    Why:
    
    This keeps my controllers small and with solo purposes. Allows me to use the resources helper for routes. Separates my views and css in a much clearer way.
    
    How:
    
    app/controllers/resources_controller.rb, app/views/resources/index.html.erb and app/assets/stylesheets/resources.css.scss have been created and utilized.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root                 'pages#home'
-  get 'resources'   => 'pages#resources'
   get 'about'       => 'pages#about'
   get 'contact'     => 'pages#contact'
   get 'volunteer'   => 'pages#volunteer'
   get 'newsletters' => 'pages#newsletters'
 
   resources :rat_adoptions, only: [:index]
+  resources :resources, only: [:index]
 end
