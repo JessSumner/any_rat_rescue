53c16a6da8fbcff7ba0746767e4eed0087cc279f
Jessica Sumner <jess.r.sumner@gmail.com>
2015-10-08 23:00:06 -0400

    Added information to each resource
    Why:
    Checked that all resources are correct and up to date. Information added included addresses which will be useful for the user. Added space for a map view so that users can get a visual cue for each resource. Started to move each resource information into a hash. This will reduce the size of the view file.
    
    How:
    Information for each resource is now going to be stored in an array of hashes in the controller.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,19 @@
 class ResourcesController < ApplicationController
+
   def index
+    @vets_phoenix = vets_phoenix  
+  end
+
+  private
+
+  def vets_phoenix
+    [
+      {
+        name: "Dr. Bill Langhofer",
+        description: "The Scottsdale Veterinary Clinic\n7311 E Thomas Rd,\nScottsdale, AZ 85251",
+        website: "http://www.scottsdaleveterinaryclinic.com",
+        phone: "480-945-8484"
+      }
+    ]
   end
 end
