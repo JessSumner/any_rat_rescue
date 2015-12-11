6fe6126a94b6ebd4b47a4256383e28bfa0646112
Jessica Sumner <jess.r.sumner@gmail.com>
2015-12-10 21:44:03 -0500

    Added validations to adoption application and added a font
    Why:
    It is important that a valid email address is provided for a potential adopter.
    
    This commit:
    Adds a regex for a valid email and this is included in the validation in the model.
    Begins to add some styling for the adoption form. The font for the header of the form and the nav bar have been customized.

--- app/models/adoption_application.rb
+++ app/models/adoption_application.rb
@@ -1,7 +1,10 @@
 class AdoptionApplication < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save :downcase_email
   validates :name, presence: true
   validates :address, presence: true
   validates :city, presence: true
   validates :cell, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
 end
