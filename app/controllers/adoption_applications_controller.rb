6ebb115842fb49c1a2d9431027534d0a8803e1dd
Jessica Sumner <jess.r.sumner@gmail.com>
2015-12-15 13:52:54 -0500

    Syntax changes to controller and adoption_application.css files
    This commit:
    Removes trailing whitespace.
    Shortens lines which were over 80 characters
    Amends a class name to include hyphens not underscores

--- app/controllers/adoption_applications_controller.rb
+++ app/controllers/adoption_applications_controller.rb
@@ -6,10 +6,12 @@ class AdoptionApplicationsController < ApplicationController
   def create
     @adoption_application = AdoptionApplication.new(adoption_application_params)
     if @adoption_application.save
-      flash[:success] = "Thank you for submitting an application. We will contact you shortly." 
+      flash[:success] = 
+        "Thank you for submitting an application. We will contact you shortly."
       redirect_to rat_adoptions_path
-    else 
-      flash[:error] = "Please review your application, some information is missing."
+    else
+      flash[:error] = 
+        "Please review your application, some information is missing."
       render :new
     end
   end
@@ -18,30 +20,30 @@ class AdoptionApplicationsController < ApplicationController
 
   def adoption_application_params
     params.require(:adoption_application).permit(
-        :name,
-        :address,
-        :city,
-        :cell,
-        :state,
-        :zip,
-        :email,
-        :current_rat_count,
-        :current_rat_gender,
-        :current_rat_age,
-        :previous_rat_count,
-        :previous_rat_lifespan,
-        :cause_of_death,
-        :vet_care,
-        :vet_name,
-        :cage_description,
-        :cage_location,
-        :household_members,
-        :children_ages,
-        :additional_animals,
-        :interaction_time,
-        :preferred_characteristics,
-        :current_rats,
-        :previous_rats,
-      )
+      :name,
+      :address,
+      :city,
+      :cell,
+      :state,
+      :zip,
+      :email,
+      :current_rat_count,
+      :current_rat_gender,
+      :current_rat_age,
+      :previous_rat_count,
+      :previous_rat_lifespan,
+      :cause_of_death,
+      :vet_care,
+      :vet_name,
+      :cage_description,
+      :cage_location,
+      :household_members,
+      :children_ages,
+      :additional_animals,
+      :interaction_time,
+      :preferred_characteristics,
+      :current_rats,
+      :previous_rats,
+    )
   end
 end
