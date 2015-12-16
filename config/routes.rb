3985d50a308587e1274d4ef50528fc570151bf35
Jessica Sumner <jess.r.sumner@gmail.com>
2015-12-16 14:57:49 -0500

    Removed redundancies and cleaned up routes
    Why:
    Small, simple and consistent is better. I want the app to be RESTful and easy to read and adapt.
    
    This commit:
    Moved CSS for the home page to it's own file.
    Deleted about.html.erb as the content is included on the home page.
    The view for the home page is now homes/show.html.erb
    Generated a homes controller with a show action.
    Renamed the routes so that the root is now homes#show.
    Deleted extra routes that are not needed at this time.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root                 'pages#home'
-  get 'about'       => 'pages#about'
-  get 'contact'     => 'pages#contact'
-  get 'volunteer'   => 'pages#volunteer'
-  get 'newsletters' => 'pages#newsletters'
+
+  root 'homes#show'
 
   resources :rat_adoptions, only: [:index]
   resources :resources, only: [:index]
