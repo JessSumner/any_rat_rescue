972862cacc23be305b950efa8dab3f1862dad844
Jessica Sumner <jess.r.sumner@gmail.com>
2015-12-17 13:55:44 -0500

    Hound corrections for clean up
    This commit:
    Removes empty lines from routes.
    Removes an extra semicolon from base/variables
    Adds line preceeding rule declarations
    Reduces selector depth to 2 for home.scss
    Orders properties alphabetically.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   root 'homes#show'
-
   resources :rat_adoptions, only: [:index]
   resources :resources, only: [:index]
   resources :adoption_applications, only: [:new, :create]
