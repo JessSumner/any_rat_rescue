84315b1144b15b98ecfb5d5ed2c4a3a14fce1573
Jessica Sumner <jess.r.sumner@gmail.com>
2016-01-25 21:36:14 -0500

    Freeze constant and remove whitespace
    Frozen the constant array. Indented the subsequent lines of the array.
    Removed whitespace from the mailer method.

--- app/mailers/new_adoption_application_mailer.rb
+++ app/mailers/new_adoption_application_mailer.rb
@@ -1,7 +1,7 @@
 class NewAdoptionApplicationMailer < ApplicationMailer
   def notification(adoption_application)
     @adoption_application = adoption_application
-    mail(to: admin_email, 
+    mail(to: admin_email,
          subject: "A new adoption application has been submitted")
   end
 
