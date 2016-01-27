39401640285458a8934bc4461d998e0a5216149d
Jessica Sumner <jess.r.sumner@gmail.com>
2016-01-26 21:41:26 -0500

    Added submission time of the form
    Having a submission time on the form would help the rescue keep track of when they received it so they can follow up promptly.
    
    Defined a method in app/helper that converts the created_at value to local time and then formats it to be easier for the reader.
    Had to add_template_helper to the Mailer to access these methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+
+  def time_submitted(datetime)
+    datetime.localtime.strftime("%l:%M %p")
+  end
+
+  def date_submitted(datetime)
+    datetime.localtime.strftime("%d %b %Y")
+  end
 end
--- app/mailers/new_volunteer_application_mailer.rb
+++ app/mailers/new_volunteer_application_mailer.rb
@@ -1,4 +1,6 @@
 class NewVolunteerApplicationMailer < ApplicationMailer
+  add_template_helper ApplicationHelper
+  
   def volunteer_notification(volunteer_application)
     @volunteer_application = volunteer_application
     mail(to: admin_email,
