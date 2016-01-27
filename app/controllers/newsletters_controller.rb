799ebdbe0bfc4cbec841d05524c8a4e632aef937
Jessica Sumner <jess.r.sumner@gmail.com>
2016-01-27 11:30:40 -0500

    Displaying monthly flyers
    Ammended fliers to flyers. Decided that flyers is more common and have decided this is how I am going to refer to them. All files have been ammended to use this spelling.
    Add 13 flyers to an assets file.
    Created a FLYERS constant that converts all the flyers file names to strings.
    The FLYERS constant can be called and iterated through to render the view.
    N.B the flyers are being accessed but not loading correctly in the view.

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,5 +1,22 @@
 class NewslettersController < ApplicationController
+  FLYERS = %w(
+    jan-16.pdf
+    dec-15.pdf
+    nov-15.pdf
+    oct-15.pdf
+    sep-15.pdf
+    aug-15.pdf
+    jul-15.pdf
+    jun-15.pdf
+    may-15.pdf
+    mar-15.pdf
+    feb-15.pdf
+    jan-15.pdf
+    dec-14.pdf
+  ).freeze
+
   def index
+    @flyers = NewslettersController::FLYERS
   end
 
   def show
