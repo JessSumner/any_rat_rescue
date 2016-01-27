c245259ddf58053430435a1d6ea0805934862a6c
Jessica Sumner <jess.r.sumner@gmail.com>
2016-01-27 10:24:18 -0500

    Generated controller and views for newsletters
    Newsletters controller has two methods: index and show.
    Created views for both index and show.
    Created routes for both index and show.
    Linked up the navigation bar link to the index page for newsletters.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :resources, only: [:index]
   resources :adoption_applications, only: [:new, :create]
   resources :volunteer_applications, only: [:new, :create]
+  resources :newsletters, only: [:index, :show]
   resource :volunteer, only: [:show]
   resource :donate, only: [:show]
 end
