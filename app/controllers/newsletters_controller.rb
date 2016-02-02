b931ba75b8e69bf8660b4315591e78ec330ce07b
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-02 10:35:41 -0500

    Refactor of the newsletters controller
    Why:
    Both news and flyers were calling their own set of the same methods.
    
    This commit:
    The method chain is the same and the other than the first and final methods they do not need to know which folder they are dealing with. The first method searches through the directory of a folder which is provided by an instance variable defined in the final method. The final method is setting the array of either flyers or news and so can specify which folder the method chain should begin.

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -6,43 +6,31 @@ class NewslettersController < ApplicationController
 
   private
 
-  def flyers
-    flyers_with_file_extension.map do |flyer|
-      flyer.gsub(/\.pdf/,"")
-    end
-  end
-
-  def flyers_with_file_extension
-    unsorted_flyers.sort do |date_1, date_2 |
-      Date.parse("1-#{date_2}") <=> Date.parse("1-#{date_1}")
-    end
-  end
-
-  def unsorted_flyers
-    monthly_flyers_contents.select{ |flyer| flyer =~ /.*\.pdf/i }
-  end
-
-  def monthly_flyers_contents
-    Dir.entries("app/assets/newsletters/monthly_flyers")
-  end
-
   def news
-    news_with_file_extension.map do |news|
-      news.gsub(/\.pdf/,"")
+    @file = "news"
+    newsletters_with_file_extension.map do |newsletter|
+      newsletter.gsub(/\.pdf/,"")
     end
   end
 
-  def news_with_file_extension
-    unsorted_news.sort do |date_1, date_2 |
+  def flyers
+    @file = "monthly_flyers"
+    newsletters_with_file_extension.map do |newsletter|
+      newsletter.gsub(/\.pdf/,"")
+    end
+  end
+
+  def newsletters_with_file_extension
+    unsorted_newsletters.sort do |date_1, date_2 |
       Date.parse("1-#{date_2}") <=> Date.parse("1-#{date_1}")
     end
   end
 
-  def unsorted_news
-    news_contents.select{ |news| news =~ /.*\.pdf/i }
+  def unsorted_newsletters
+    file_contents.select{ |newsletter| newsletter =~ /.*\.pdf/i }
   end
 
-  def news_contents
-    Dir.entries("app/assets/newsletters/news")
+  def file_contents
+    Dir.entries("app/assets/newsletters/#{@file}")
   end
 end
