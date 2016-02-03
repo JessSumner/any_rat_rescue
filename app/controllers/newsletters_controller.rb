9f9a12e3467852af293c87fbd313e2b7e8af9c63
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-03 10:22:11 -0500

    Removed the instance variables defined in the controller.
    It is good practice to avoid using instance variables within the controller.
    
    I have updated the methods to take an argument and therefore eliminate the need for defining the instance variable.

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -7,30 +7,28 @@ class NewslettersController < ApplicationController
   private
 
   def news
-    @file = "news"
-    newsletters_with_file_extension.map do |newsletter|
+    newsletters_with_file_extension("news").map do |newsletter|
       newsletter.gsub(/\.pdf/, "")
     end
   end
 
   def flyers
-    @file = "monthly_flyers"
-    newsletters_with_file_extension.map do |newsletter|
+    newsletters_with_file_extension("monthly_flyers").map do |newsletter|
       newsletter.gsub(/\.pdf/, "")
     end
   end
 
-  def newsletters_with_file_extension
-    unsorted_newsletters.sort do |date_1, date_2|
+  def newsletters_with_file_extension(file_name)
+    unsorted_newsletters(file_name).sort do |date_1, date_2|
       Date.parse("1-#{date_2}") <=> Date.parse("1-#{date_1}")
     end
   end
 
-  def unsorted_newsletters
-    file_contents.select { |newsletter| newsletter =~ /.*\.pdf/i }
+  def unsorted_newsletters(file_name)
+    file_contents(file_name).select { |newsletter| newsletter =~ /.*\.pdf/i }
   end
 
-  def file_contents
-    Dir.entries("app/assets/newsletters/#{@file}")
+  def file_contents(file_name)
+    Dir.entries("app/assets/newsletters/#{file_name}")
   end
 end
