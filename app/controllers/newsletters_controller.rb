9c2db1192282fa3fdbcaafe9dd535d493ead538c
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-04 12:32:56 -0500

    Changed folder name
    Why:
    I was using "news" for the folder name and it's contents.
    
    How:
    Replaced news in the folder name, the NewsletterController and the index view.
    
    Lack of side-effect:
    Nothing needed to be changed in the NewsletterFinder object. This was the goal of making this object and separating the logic.

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,6 +1,6 @@
 class NewslettersController < ApplicationController
   def index
     @flyers = NewsletterFinder.new("monthly_flyers")
-    @news = NewsletterFinder.new("news")
+    @news = NewsletterFinder.new("news_documents")
   end
 end
