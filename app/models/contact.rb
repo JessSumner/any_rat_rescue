8205cec5855ed95e7cab19f52184a1a62b48f79c
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-08 14:13:13 -0500

    Set up infrastructure for Contact Us page
    Generated a model.
    Database table created with columns for name, email, phone and message.
    Validations for these columns as all fields should be filled out. Validations for format for email and phone.
    Generated a controller.

---
+++ app/models/contact.rb
@@ -0,0 +1,12 @@
+class Contact < ActiveRecord::Base
+  before_save :downcase_email
+  validates :name, presence: true
+  validates :cell, cell: true, presence: true
+  validates :email, email: true, presence: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
