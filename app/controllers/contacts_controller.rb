ee917f44e7757044042ccc359a27ec01fad455f2
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-08 16:13:03 -0500

    Added create method and errors
    Create method to show flash message if the message gets saved. If the message is incorrectly filled out a flash message will appear and the visitor will be directed back to their form.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,26 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] =
+        "It's good to hear from you. We will respond to your message shortly."
+      redirect_to root_path
+    else
+      flash.now[:error] =
+        "Please review your message, some information is missing."
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(
+      :name,
+      :email,
+      :cell,
+      :message
+    )
   end
 end
