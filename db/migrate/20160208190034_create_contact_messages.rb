d7b12230f59be84d84a09ae7e5b9713789eab5f4
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-10 10:30:37 -0500

    Removed color literals and whitespace
    Migration file had trailing whitespace. This has been removed.
    The box shadow was using a rgb color literal. This has been extracted into a variable. $black is now defined and $shadow is defined as 75% transparent black.

--- db/migrate/20160208190034_create_contact_messages.rb
+++ db/migrate/20160208190034_create_contact_messages.rb
@@ -5,7 +5,7 @@ class CreateContactMessages < ActiveRecord::Migration
       t.string :email, null: false
       t.string :cell, null: false
       t.text :message, null: false
-      
+
       t.timestamps null: false
     end
   end
