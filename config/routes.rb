a98c0a9d696c5dc1785ee952379a53e042c707b0
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-10 21:58:28 -0500

    Corrections and organizations
    Deleted the contacts helper file as it is empty.
    Organized the routes resources to be alphabetical.
    Added a semicolon at the end of property declaration.
    Removed trailing slash from html preview.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'homes#show'
+  resources :adoption_applications, only: [:new, :create]
+  resources :contact_messages, only: [:new, :create]
+  resources :newsletters, only: [:index]
   resources :rat_adoptions, only: [:index]
   resources :resources, only: [:index]
-  resources :adoption_applications, only: [:new, :create]
   resources :volunteer_applications, only: [:new, :create]
-  resources :newsletters, only: [:index]
-  resources :contact_messages, only: [:new, :create]
-  resource :volunteer, only: [:show]
   resource :donate, only: [:show]
+  resource :volunteer, only: [:show]
 end
